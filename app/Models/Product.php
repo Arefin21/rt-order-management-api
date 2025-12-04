59c98891ca1ecdc918de6a16750356be827d284a
Arefin21 <arefinshagor18@gmail.com>
2025-12-05 01:51:40 +0600

    stock: add Stock model, migration,seeders

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -20,4 +21,12 @@ class Product extends Model
         'barcode',
         'slug',
     ];
+
+    /**
+     * Get the stocks for the product.
+     */
+    public function stocks(): HasMany
+    {
+        return $this->hasMany(Stock::class);
+    }
 }
